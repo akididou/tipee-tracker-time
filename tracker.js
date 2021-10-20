8c0f9aa20af88de525d4ff50d11d8e6e48a75216
jean-patrick <jean-patrick.poizat@infomaniak.com>
2021-10-20 17:23:09 +0200

    Add style

--- tracker.js
+++ tracker.js
@@ -1,117 +1,97 @@
 /* -------------------------------------------------------------------------- */
-/*                                 USER CONFIG                                */
-/* -------------------------------------------------------------------------- */
-var products = [
-  '6800 XT',
-  '6900 XT',
-  // '3950X',
-] // Names of products searched
-var timer = 1000 * 30; // Duration of checking in milliseconds
-
-/* -------------------------------------------------------------------------- */
 /*                              EXTENSION CONFIG                              */
 /* -------------------------------------------------------------------------- */
-var titles = document.querySelectorAll('.shop-title');
-var indexTitle = {};
-var elementExist = false
-var hasPermissionNotification = false;
 
-var interval = setInterval(() => {
-  console.log('Test ping : ' + formattedDate())
-  var verif = false;
-  setCookie('amd-ping', formattedDate());
-  // Close if a product is already in cart
-  if (document.querySelector('.fa-shopping-cart').hasAttribute('data-count')) {
-    console.log('clearInterval')
-    clearInterval(interval);
-  }
-
-  // Request notification permission
-  Notification.requestPermission((permission) => {
-    hasPermissionNotification = permission === 'granted'
-    if (hasPermissionNotification && !getCookie('testNotification')) {
-      setCookie('amd-notification', true);
-      new Notification(`Notification test`, {
-        body: `Just to check that permissions are allowed`,
-        timestamp: Math.floor(Date.now()),
-        renotify: true,
-        tag: 'adm-tracker'
-      })
-    }
-  });
-
-  // Find products
-  titles.forEach((title, index) => {
-    products.forEach(product => {
-      if (title.textContent.includes(product)) {
-        indexTitle = {
-          ...indexTitle,
-          [product]: index
-        }
-      }
+function getUser() {
+  return fetch('/brain/users/me')
+    .then((resp) => resp.json())
+    .then((data) => {
+      return data;
     })
-  })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
-  Object.keys(indexTitle).forEach((index) => {
-    if (indexTitle[index] >= 0) {
-      var tempDom = document.querySelectorAll('.shop-title')[indexTitle[index]].parentElement
-      if (tempDom.querySelector('.shop-links button')) {
-        console.log(`${index} is available at ${formattedDate()}`)
-        verif = true;
-        if (hasPermissionNotification) {
-          clearInterval(interval);
-          tempDom.querySelector('.shop-links button').click();
-          var notification = new Notification(`${index} is available !!!`, {
-            body: `${index} is available at ${formattedDate()}`,
-            timestamp: Math.floor(Date.now()),
-            renotify: true,
-            tag: 'adm-tracker'
-          })
-          notification.addEventListener('click', (event) => {
-            event.preventDefault(); // prevent the browser from focusing the Notification's tab
-            window.open('https://www.amd.com/fr/direct-buy/fr', '_blank');
-          })
+function getWorkday(user) {
+  return fetch(`/api/employees/${user.id}/workday`)
+    .then((resp) => resp.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
-          setCookie(`amd-${index}-available`, formattedDate());
-          window.open('https://www.amd.com/fr/direct-buy/fr', '_blank');
-        }
-      } else {
-        console.log(`${index} isn't available at ${formattedDate()}`)
-      }
-    } else {
-      console.warn('Product ' + index + 'do not exist on this page.')
-    }
-  })
+function beautifyTime(minutes) {
+  return (Math.abs(minutes) / 60) > 1 ? (Math.floor(Math.abs(minutes) / 60)) + 'h' + (Math.abs(minutes) % 60) + 'min' :
+    '0h' + Math.abs(minutes) + 'min';
+}
 
-  if (!verif) {
-    document.location.reload();
+function toMinutes(s) {
+  const a = s.split(':');
+  return (+a[0]) * 60 + (+a[1]);
+};
+
+
+function getRemaining(init) {
+  getUser()
+    .then((user) => {
+      getWorkday(user)
+        .then(result => {
+          let text = '';
+          let date = new Date();
+          let x = (60 * 8) - (+(result.timechecks
+            .map(x => x.duration > 0 ?
+              Math.floor(x.duration) :
+              toMinutes(date.getHours() + ':' + date.getMinutes()) - toMinutes(x.hour_in))
+            .reduce((a, b) => a + b, 0)));
+
+          let hourToDo = Math.floor(x / 60);
+          let minuteToDo = x % 60;
+          minuteToDo = result.timechecks[0].time_out === null ? minuteToDo + 30 : minuteToDo;
+          let endHour = date.getHours() + hourToDo;
+          let endMinute = date.getMinutes() + minuteToDo;
+          if (endMinute >= 60) {
+            endHour++;
+            endMinute -= 60;
+          }
+          if (x > 0) {
+            text = beautifyTime(x) + ' left (A ' + endHour + 'H' + endMinute + ')';
+          } else {
+            text = 'Felicitation, you can go ! (You did : ' + beautifyTime(-x) + ' of overtime)';
+          }
+
+          if (init) {
+            const containerDiv = document.createElement('div');
+            containerDiv.setAttribute('id','containerTimer');
+            containerDiv.style.cssText = 'display:flex;';
+            document.querySelector('.nav.navbar-toolbar.navbar-toolbar-left').appendChild(containerDiv);
+
+            const textDiv = document.createElement('div');
+            textDiv.innerHTML = text;
+            textDiv.setAttribute('id','textTimer');
+            textDiv.style.cssText = 'display: flex;width: 100%;padding: 15px;z-index: 100;background: #f1f1f1;color: #333;align-items: center;border: 0;border-radius: 4px;';
+            document.querySelector('#containerTimer').appendChild(textDiv);
+            
+            const btnDiv = document.createElement("BUTTON");
+            btnDiv.innerText = 'Refresh';
+            btnDiv.style.cssText = 'display: flex;padding: 15px;z-index: 100;background: #f1f1f1;color: #333;border: 0;margin-left: 10px;align-items: center;border-radius: 4px;height: 100%;';
+            btnDiv.onclick = function () { getRemaining() };
+            document.querySelector('#containerTimer').appendChild(btnDiv);
+
+            // btnDiv.appendChild(buttonTitle);
+            // const buttonTitle = document.createTextNode("Refresh"); // Create a text node
+          } else {
+            document.querySelector('#textTimer').innerText = text;
+          }
+        })
+    })
+}
+
+document.onreadystatechange = (e) => {
+  if (document.readyState == "complete") {
+    getRemaining(true);
   }
-}, timer)
-
-// Format Date 
-function formattedDate(d = new Date) {
-  let month = String(d.getMonth() + 1);
-  let day = String(d.getDate());
-  let hours = String(d.getHours());
-  let minutes = String(d.getMinutes());
-  let seconds = String(d.getSeconds());
-  const year = String(d.getFullYear());
-
-  if (month.length < 2) month = '0' + month;
-  if (day.length < 2) day = '0' + day;
-  if (hours.length < 2) hours = '0' + hours;
-  if (minutes.length < 2) minutes = '0' + minutes;
-  if (seconds.length < 2) seconds = '0' + seconds;
-
-  return `${day}/${month}/${year} - ${hours}:${minutes}:${seconds}`;
-}
-
-function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
-}
-
-function setCookie(name, value) {
-  document.cookie = name + "=" + value + ";path=/;";
 }
